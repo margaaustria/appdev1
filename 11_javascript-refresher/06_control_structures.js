fca5c015e34b9cc4628426a2ed50e9fa110b3c6b
Marga Austria <calumpianomargarette@gmail.com>
2025-09-05 22:09:29 +0800

    Add control structures activity

---
+++ 11_javascript-refresher/06_control_structures.js
@@ -0,0 +1,21 @@
+let grade = 75
+
+if (grade >= 90) {
+    console.log("Grade: A")
+}  else if (grade >= 80) {
+    console.log("Grade: B")
+} else if (grade >= 70) {
+    console.log("Grade: C")
+} else {
+    console.log("Grade: F")
+}
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i)
+}
+
+let count = 0
+while (count < 3) {
+    console.log("Hello")
+    count++
+}
