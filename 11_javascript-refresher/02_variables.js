1f6112bf541c08063fa5db5db95d00a50fed5ed6
Marga Austria <calumpianomargarette@gmail.com>
2025-09-05 22:07:21 +0800

    Add variables activity

---
+++ 11_javascript-refresher/02_variables.js
@@ -0,0 +1,21 @@
+let name = "Marga"
+let age = 20
+let isStudent = true
+
+console.log ("Name:", name)
+console.log("Age:", age)
+console.log("Is Student:", isStudent)
+
+console.log(typeof name) 
+console.log(typeof age)   
+console.log(typeof isStudent)
+
+let num1 = 20
+let num2 = 4
+console.log("Add:", num1 + num2)
+console.log("Subtract:", num1 - num2)
+console.log("Multiply:", num1 * num2)
+console.log("Divide:", num1 / num2)
+
+console.log('"5" == 5:', "5" == 5)
+console.log('"5" === 5:', "5" === 5)
