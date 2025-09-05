c43a00b290af675d48eace7b773fe82749472f4c
Marga Austria <calumpianomargarette@gmail.com>
2025-09-05 22:08:04 +0800

    Add functions activity

---
+++ 11_javascript-refresher/03_functions.js
@@ -0,0 +1,14 @@
+function greet(name) {
+    return "Hello, " + name 
+}
+console.log(greet( "Marga!"))
+
+const square = num => num * num
+console.log(square(20))
+
+function calculator(a, b) {
+    console.log("Sum:", (a + b))
+    console.log("Product:", (a * b))
+}
+
+calculator(20, 5)
