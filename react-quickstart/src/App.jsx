7cb156c7bb711a04a16aeec602a5b941e33f0782
Margarette Austria <calumpianomargarette@gmail.com>
2025-09-17 23:03:50 +0800

    Done adding styles

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function AboutPage() {
   return (
     <>
-      <h1>About</h1>
-      <p>Hello there.<br />How do you do?</p>
+      <h1>About Page</h1>
+      <img
+        className="avatar"
+        src="https://i.imgur.com/yXOvdOSs.jpg"
+        alt="Profile"
+      />
     </>
   );
 }
-export default AboutPage
+
+export default AboutPage;
