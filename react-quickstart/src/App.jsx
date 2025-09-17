702b4973d59e4eb4295259edfa08729fd3fad971
Margarette Austria <calumpianomargarette@gmail.com>
2025-09-17 22:49:05 +0800

    Done creating and nesting components

---
+++ react-quickstart/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  )
+}
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  )
+
+}
+
+
+export default MyApp
