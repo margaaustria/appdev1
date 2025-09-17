c78e88a47d19c72fef7969373eacc5414a60f3ad
Margarette Austria <calumpianomargarette@gmail.com>
2025-09-17 23:05:42 +0800

    Done displaying data

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,14 +1,30 @@
-function AboutPage() {
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+function Profile() {
   return (
     <>
-      <h1>About Page</h1>
+      <h1>{user.name}</h1>
       <img
         className="avatar"
-        src="https://i.imgur.com/yXOvdOSs.jpg"
-        alt="Profile"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
       />
     </>
-  );
+  )
 }
 
-export default AboutPage;
+
+export default Profile
