b7a13164837df31f77d70bc0c88a72815d08282d
Margarette Austria <calumpianomargarette@gmail.com>
2025-11-01 12:42:45 +0800

    Done with why understanding your UI as trees is useful

--- react-describing-ui/src/main.jsx
+++ react-describing-ui/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import RootComponent from './components/ComponentA.jsx'
+import RootComponent from './RootComponent.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
