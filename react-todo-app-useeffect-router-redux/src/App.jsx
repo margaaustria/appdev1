1de91b0a123c2eceb4885385587f55f18313ba96
margaaustria <calumpianomargarette@gmail.com>
2025-11-27 23:40:22 +0800

    Done with creating the Home page

--- react-todo-app-useeffect-router-redux/src/App.jsx
+++ react-todo-app-useeffect-router-redux/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home"
-import Login from "./pages/Login"
-import Todos from "./pages/Todos"
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Todos from './pages/Todos';
 
 function App () {
   return (
